256fa471b4987d9621cbec9e7ad99a161c5d8573
VISTALL <vistall.valeriy@gmail.com>
2015-06-16 18:53:36 +0300

    enable extensino while maven importing

---
+++ maven-impl/src/org/mustbe/consulo/stapler/maven/StaplerMavenImporter.java
@@ -0,0 +1,48 @@
+package org.mustbe.consulo.stapler.maven;
+
+import java.util.List;
+import java.util.Map;
+
+import org.jetbrains.idea.maven.importing.MavenImporterFromBuildPlugin;
+import org.jetbrains.idea.maven.importing.MavenModifiableModelsProvider;
+import org.jetbrains.idea.maven.importing.MavenRootModelAdapter;
+import org.jetbrains.idea.maven.project.MavenProject;
+import org.jetbrains.idea.maven.project.MavenProjectChanges;
+import org.jetbrains.idea.maven.project.MavenProjectsProcessorTask;
+import org.jetbrains.idea.maven.project.MavenProjectsTree;
+import org.mustbe.consulo.stapler.module.extension.StaplerModuleExtension;
+import com.intellij.openapi.module.Module;
+
+/**
+ * @author VISTALL
+ * @since 16.06.2015
+ */
+public class StaplerMavenImporter extends MavenImporterFromBuildPlugin
+{
+	public StaplerMavenImporter()
+	{
+		super("org.kohsuke.stapler", "maven-stapler-plugin");
+	}
+
+	@Override
+	public void preProcess(Module module,
+			MavenProject mavenProject,
+			MavenProjectChanges mavenProjectChanges,
+			MavenModifiableModelsProvider mavenModifiableModelsProvider)
+	{
+
+	}
+
+	@Override
+	public void process(MavenModifiableModelsProvider mavenModifiableModelsProvider,
+			Module module,
+			MavenRootModelAdapter mavenRootModelAdapter,
+			MavenProjectsTree mavenProjectsTree,
+			MavenProject mavenProject,
+			MavenProjectChanges mavenProjectChanges,
+			Map<MavenProject, String> mavenProjectStringMap,
+			List<MavenProjectsProcessorTask> mavenProjectsProcessorTasks)
+	{
+		enableModuleExtension(module, mavenModifiableModelsProvider, StaplerModuleExtension.class);
+	}
+}
