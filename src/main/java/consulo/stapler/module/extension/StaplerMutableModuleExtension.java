1d54780f8a4978ce9dba0e8ea4d7c5c60fab723d
VISTALL <vistall.valeriy@gmail.com>
2020-06-03 20:59:17 +0300

    compilation

--- src/main/java/consulo/stapler/module/extension/StaplerMutableModuleExtension.java
+++ src/main/java/consulo/stapler/module/extension/StaplerMutableModuleExtension.java
@@ -1,9 +1,13 @@
 package consulo.stapler.module.extension;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
+import consulo.disposer.Disposable;
 import consulo.module.extension.MutableModuleExtension;
 import consulo.roots.ModuleRootLayer;
+import consulo.ui.Component;
+import consulo.ui.annotation.RequiredUIAccess;
 
 /**
  * @author VISTALL
@@ -14,6 +18,14 @@ public class StaplerMutableModuleExtension extends StaplerModuleExtension implem
 	public StaplerMutableModuleExtension(@Nonnull String id, @Nonnull ModuleRootLayer moduleRootLayer)
 	{
 		super(id, moduleRootLayer);
+	}
+
+	@RequiredUIAccess
+	@Nullable
+	@Override
+	public Component createConfigurationComponent(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
+	{
+		return null;
 	}
 
 	@Override
